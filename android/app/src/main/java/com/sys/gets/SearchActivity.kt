21f2e4e20f0a3553383321dec1f67b93b6034d91
LeeSeungYun1020 <ileilliat@gmail.com>
2021-05-10 19:45:12 +0900

    Add search activity.

---
+++ android/app/src/main/java/com/sys/gets/SearchActivity.kt
@@ -0,0 +1,12 @@
+package com.sys.gets
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+        actionBar?.setDisplayShowHomeEnabled(true)
+    }
+}
