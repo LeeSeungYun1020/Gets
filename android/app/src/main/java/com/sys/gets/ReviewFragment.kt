a58393955e83fa6ce4905bbd5229b421f418b891
LeeSeungYun1020 <ileilliat@gmail.com>
2021-05-13 23:28:17 +0900

    Add review fragment and related layouts.

---
+++ android/app/src/main/java/com/sys/gets/ReviewFragment.kt
@@ -0,0 +1,31 @@
+package com.sys.gets
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+
+class ReviewFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = ReviewFragment()
+    }
+
+    private lateinit var viewModel: ReviewViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_review, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(ReviewViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ android/app/src/main/java/com/sys/gets/ReviewViewModel.kt
@@ -0,0 +1,7 @@
+package com.sys.gets
+
+import androidx.lifecycle.ViewModel
+
+class ReviewViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
