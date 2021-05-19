bdfc47e31bbcd0f7d5d1e7391308aa9fc059d83d
dabinchiii <dabinchi388@naver.com>
2021-05-19 16:41:08 +0900

    Add items to a coordination recycler view

--- android/app/src/main/java/com/sys/gets/ui/coordination/CoordinationFragment.kt
+++ android/app/src/main/java/com/sys/gets/ui/coordination/CoordinationFragment.kt
@@ -8,12 +8,18 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.sys.gets.R
 import com.sys.gets.databinding.FragmentCoordinationBinding
 
 class CoordinationFragment : Fragment() {
 
     private lateinit var coordinationViewModel: CoordinationViewModel
     private var _binding: FragmentCoordinationBinding? = null
+
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<CoordinationRecyclerAdapter.ViewHolder>? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -38,4 +44,27 @@ class CoordinationFragment : Fragment() {
         _binding = null
     }
 
+    override fun onViewCreated(itemView: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(itemView, savedInstanceState)
+        var recycler_view: RecyclerView = itemView.findViewById(R.id.codyRecyclerview)
+        var codyItemList = arrayListOf<CoordinationItem>(
+            //임의 데이터
+            CoordinationItem("cody_example","cody1"),
+            CoordinationItem("cody_example","cody2"),
+            CoordinationItem("cody_example","cody3"),
+            CoordinationItem("cody_example","cody4"),
+            CoordinationItem("cody_example","cody5"),
+            CoordinationItem("cody_example","cody6"),
+            CoordinationItem("cody_example","cody7"),
+            CoordinationItem("cody_example","cody8")
+        )
+
+        recycler_view.apply {
+            // set a GridLayoutManager to handle Android
+            // RecyclerView behavior
+            layoutManager = GridLayoutManager(activity, 2)
+            // set the custom adapter to the RecyclerView
+            adapter = CoordinationRecyclerAdapter(context, codyItemList)
+        }
+    }
 }
---
+++ android/app/src/main/java/com/sys/gets/ui/coordination/CoordinationItem.kt
@@ -0,0 +1,7 @@
+package com.sys.gets.ui.coordination
+
+import android.graphics.drawable.Drawable
+
+class CoordinationItem(val imageId: String, val title: String) {
+//데이터를 저장하고 getter/setter만 제공하면 되기 때문에 추가로 구현할 것은 없다.
+}
