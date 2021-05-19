1775f6a232e11383368937d0d60de69796fe6e06
LeeSeungYun1020 <ileilliat@gmail.com>
2021-05-19 14:22:06 +0900

    Add api description at API page.

--- server/public/javascripts/strings.js
+++ server/public/javascripts/strings.js
@@ -7,7 +7,14 @@ const strings = {
         closet: "Closet",
         coordination: "Coordination",
         redirect_api: "goto API",
-        api_title: "Gets: API"
+        api_title: "Gets: API",
+        api_sign: "Sign in/up",
+        api_sign_in: "Sign in",
+        api_sign_up: "Sign up",
+        api_sign_up_basic: "Sign up - basic",
+        api_sign_up_info: "Sign up - info",
+        api_sign_up_check: "Sign up - check id",
+        api_weather: "Weather",
     },
     ko: {
         title: "Gets",
@@ -17,7 +24,14 @@ const strings = {
         closet: "옷장",
         coordination: "코디",
         redirect_api: "API 바로가기",
-        api_title: "Gets: API"
+        api_title: "Gets: API",
+        api_sign: "로그인/회원가입",
+        api_sign_in: "로그인",
+        api_sign_up: "회원가입",
+        api_sign_up_basic: "회원가입 - 기본",
+        api_sign_up_info: "회원가입 - 추가 정보",
+        api_sign_up_check: "회원가입 - ID 중복 확인",
+        api_weather: "날씨",
     }
 }
 
