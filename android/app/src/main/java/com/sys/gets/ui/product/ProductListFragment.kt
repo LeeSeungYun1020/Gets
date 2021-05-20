15c656a0316537a78b1490f6d767aafc6ca906e1
LeeSeungYun1020 <ileilliat@gmail.com>
2021-05-20 23:22:30 +0900

    Modify product list fragment

--- android/app/src/main/java/com/sys/gets/ui/product/ProductListFragment.kt
+++ android/app/src/main/java/com/sys/gets/ui/product/ProductListFragment.kt
@@ -2,8 +2,12 @@ package com.sys.gets.ui.product
 
 import android.os.Bundle
 import android.view.*
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.sys.gets.R
 import com.sys.gets.databinding.FragmentProductListBinding
 
@@ -24,13 +28,26 @@ class ProductListFragment : Fragment() {
     ): View? {
         _binding = FragmentProductListBinding.inflate(inflater, container, false)
         setHasOptionsMenu(true)
+        binding.root.apply {
+            layoutManager = GridLayoutManager(requireContext(), 2)
+            adapter = ProductListAdapter(
+                listOf(
+                    // TODO: test data
+                    ProductSimple("img", "Name1", "Brand1", "30,000"),
+                    ProductSimple("img", "Name2", "Brand2", "10,000"),
+                    ProductSimple("img", "Name3", "Brand3", "20,000"),
+                    ProductSimple("img", "Name4", "Brand4", "40,000"),
+                    ProductSimple("img", "Name5", "Brand5", "530,000")
+                )
+            )
+        }
         return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(ProductListViewModel::class.java)
-        // TODO: Use the ViewModel
+
     }
 
     override fun onDestroyView() {
@@ -51,4 +68,35 @@ class ProductListFragment : Fragment() {
             else -> super.onOptionsItemSelected(item)
         }
     }
+}
+
+class ProductListAdapter(private val dataSet: List<ProductSimple>) :
+    RecyclerView.Adapter<ProductListAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val image: ImageView = view.findViewById(R.id.product_preview_image)
+        val name: TextView = view.findViewById(R.id.product_preview_name)
+        val brand: TextView = view.findViewById(R.id.product_preview_brand)
+        val price: TextView = view.findViewById(R.id.product_preview_price)
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.card_product, viewGroup, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        val data = dataSet[position]
+        viewHolder.apply {
+            image.setImageResource(R.drawable.clothing_example)
+            name.text = data.name
+            brand.text = data.brand
+            price.text = data.price
+        }
+    }
+
+    override fun getItemCount() = dataSet.size
+
 }
