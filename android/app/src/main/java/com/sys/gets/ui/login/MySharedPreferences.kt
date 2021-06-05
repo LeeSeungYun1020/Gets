d14cc9beff9b5cf34887e6d77d590690d2df1d5d
gimume <yum17201619@gmail.com>
2021-06-05 11:21:50 +0900

    Modify login snack bar and save login information

---
+++ android/app/src/main/java/com/sys/gets/ui/login/MySharedPreferences.kt
@@ -0,0 +1,39 @@
+package com.sys.gets.ui.login
+
+import android.content.Context
+import android.content.SharedPreferences
+
+object MySharedPreferences {
+    private val MY_ACCOUNT : String = "account"
+
+    fun setUserId(context: Context, input: String) {
+        val prefs : SharedPreferences = context.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE)
+        val editor : SharedPreferences.Editor = prefs.edit()
+        editor.putString("MY_ID", input)
+        editor.commit()
+    }
+
+    fun getUserId(context: Context): String {
+        val prefs : SharedPreferences = context.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE)
+        return prefs.getString("MY_ID", "").toString()
+    }
+
+    fun setUserPass(context: Context, input: String) {
+        val prefs : SharedPreferences = context.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE)
+        val editor : SharedPreferences.Editor = prefs.edit()
+        editor.putString("MY_PASS", input)
+        editor.commit()
+    }
+
+    fun getUserPass(context: Context): String {
+        val prefs : SharedPreferences = context.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE)
+        return prefs.getString("MY_PASS", "").toString()
+    }
+
+    fun clearUser(context: Context) {
+        val prefs : SharedPreferences = context.getSharedPreferences(MY_ACCOUNT, Context.MODE_PRIVATE)
+        val editor : SharedPreferences.Editor = prefs.edit()
+        editor.clear()
+        editor.commit()
+    }
+}
