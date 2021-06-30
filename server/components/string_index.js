55695f2ccf3a899d94d0fe02bf7222b0c6dade02
gimume <yum17201619@gmail.com>
2021-06-30 15:45:38 +0900

    modify header css

--- server/components/string_index.js
+++ server/components/string_index.js
@@ -4,12 +4,37 @@ const string = {
 		main_title: "Gets: Custom Fashion Platform",
 		select_style: "#Select your style",
 		recommend_style: "#Recommend style",
+		select_info: "#About",
+		gender: "GENDER",
+		age: "AGE",
+		body_type: "BODY TYPE",
+		fit: "FIT",
+		woman: "woman",
+		man: "man",
+		age_10: "10s",
+		age_20: "20s",
+		age_30: "30s",
+		age_40: "40s",
+		age_50: "50s ~",
+		
+		
+		
 	},
 	ko: {
 		title: "Gets",
 		main_title: "Gets: 맟춤형 패션 플랫폼",
 		select_style: "#취향 선택",
 		recommend_style: "#추천 코디",
+		select_info: "#About",
+		gender: "성별",
+		age: "나이",
+		body_type: "체형",
+		fit: "핏",
+		age_10: "10대",
+		age_20: "20대",
+		age_30: "30대",
+		age_40: "40대",
+		age_50: "50대 ~",
 	}
 }
 
