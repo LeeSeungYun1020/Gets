6fe58f20ffbd4aad70a40b6446ead5d8fd6f8d87
LeeSeungYun1020 <ileilliat@gmail.com>
2021-07-02 16:03:23 +0900

    Add footer string resources

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,10 +4,11 @@ const router = express.Router();
 // components
 const commonHead = require('../components/commonHead')
 const string = require('../components/string_index')
+const fstring = require('../components/string_footer')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', {commonHead: commonHead, string: string[req.body.locale]})
+  res.render('index', {commonHead: commonHead, string: string[req.body.locale], fstring: fstring[req.body.locale]})
 });
 
 module.exports = router;
