827f98d32184a107095e26d1820be2fcc437a79d
LeeSeungYun1020 <ileilliat@gmail.com>
2021-07-07 23:43:24 +0900

    Add internalization language support

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './stylesheets/App.scss';
+import {useTranslation} from 'react-i18next'
 import Header from './components/Header.js'
 
 function App() {
+    const {t, i18n} = useTranslation()
     return (
         <div className="App">
             <Header/>
+            <div>{t("test")}</div>
         </div>
     )
 }
