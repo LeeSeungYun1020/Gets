3a9a2ae60187e3275b6c81c632e77f1cb5685d4d
LeeSeungYun1020 <ileilliat@gmail.com>
2021-07-08 12:27:32 +0900

    Add home recommend selection control

--- web/src/components/HomeRecommend.js
+++ web/src/components/HomeRecommend.js
@@ -1,18 +1,46 @@
 import HomeRecommendItem from "./HomeRecommendItem";
+import React from "react";
 
-function HomeRecommend(props) {
-    const chips = props.chips.map((item) =>
-        <HomeRecommendItem key={item.text} image={item.image} text={item.text}/>
-    )
+class HomeRecommend extends React.Component {
+    constructor(props) {
+        super(props);
+        let isSelected = []
+        for (let item of this.props.chips) {
+            isSelected[item.text] = item.selected
+        }
+        this.state = {selected: isSelected}
+    }
 
-    return (
-        <div id="recommend">
-            <h1>{props.title}</h1>
-            <div className="style">
-                {chips}
+    styleClick(key) {
+        let isSelected = []
+        for (let i = 0; i < this.props.chips.length; i++) {
+            if (this.props.chips[i].text === key) {
+                this.props.chips[i].selected = !this.props.chips[i].selected
+            }
+            isSelected[this.props.chips[i].text] = this.props.chips[i].selected
+        }
+        this.setState({
+            selected: isSelected
+        })
+    }
+
+    render() {
+        const chips = this.props.chips.map((item) =>
+            <div key={item.text} onClick={this.styleClick.bind(this, item.text)}>
+                <HomeRecommendItem image={item.image} text={item.text} selected={item.selected}/>
             </div>
-        </div>
-    )
+        )
+        return (
+            <div id="recommend">
+                <h1>{this.props.title}</h1>
+                <div className="style">
+                    {chips}
+                </div>
+            </div>
+        )
+    }
+
+
 }
 
 export default HomeRecommend
--- web/src/components/HomeRecommendItem.js
+++ web/src/components/HomeRecommendItem.js
@@ -1,7 +1,18 @@
+function SelectedItemImage(props) {
+    return <img className="style_chip style_selected" src={props.image}/>
+}
+
+function DeselectedItemImage(props) {
+    return <img className="style_chip style_deselected" src={props.image}/>
+}
+
 function HomeRecommendItem(props) {
+    let itemImage = <DeselectedItemImage image={props.image}/>
+    if (props.selected)
+        itemImage = <SelectedItemImage image={props.image}/>
     return (
         <div>
-            <img className="style_chip" src={props.image}/>
+            {itemImage}
             <div className="style_text">{props.text}</div>
         </div>
     )
