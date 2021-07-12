121e425498b5eef6030f80d9d4e10668cbccf797
LeeSeungYun1020 <ileilliat@gmail.com>
2021-07-12 14:46:45 +0900

    Modify input algorithm

--- server/routes/input.js
+++ server/routes/input.js
@@ -10,11 +10,12 @@ router.get('/', function(req, res, next) {
 		let records = []
 		let index = 1
 		const parser = fs
-		.createReadStream(`./product/product.csv`)
+		.createReadStream(`./product/product1.csv`)
 		.pipe(parse({
 			// CSV options
 		}));
 		for await (const record of parser) {
+			const id = record[0]
 			const name = record[1]
 			const brand = record[2]
 			const code = record[3]
@@ -28,16 +29,17 @@ router.get('/', function(req, res, next) {
 			const age = product.getAgeCode(record[18])
 			const style = product.getStyleCode(record[19])
 			const price = record[20]
-			const image = record[23].split(",")
+			const size = record[21]
+			const image = record[24].split(",")
 			// TODO: 이미지 처리
 			connection.query("insert into product \
-				(name, brand, code, gender, type, detail, color, fit, season, fiber, age, style, price, image1ID, image2ID, image3ID) \
-				values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-				[name, brand, code, gender, type, detail, color, fit, season, fiber, age, style, price, image[0], image[1] || null, image[2] || null],
+				(id, name, brand, code, gender, type, detail, color, fit, season, fiber, age, style, price, image1ID, image2ID, image3ID) \
+				values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+				[id, name, brand, code, gender, type, detail, color, fit, season, fiber, age, style, price, image[0], image[1] || null, image[2] || null],
 				(err, result) => {
-				if (err)
-					console.error(err)
-			})
+					if (err)
+						console.error(err)
+				})
 			records.push(record)
 		}
 		return records
