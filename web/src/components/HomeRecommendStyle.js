32ffa60dadaa4fdcf38f2395a0bb9f35bfb3bc8f
gimume <yum17201619@gmail.com>
2021-07-12 11:01:26 +0900

    add recommend style card

--- web/src/components/HomeRecommendStyle.js
+++ web/src/components/HomeRecommendStyle.js
@@ -1,16 +1,34 @@
 import React from "react";
-
+import HomeRecommendCard from "./HomeRecommendCard";
+import style_example from "../images/home/style_example.jpg"
 class HomeRecommendStyle extends React.Component {
     constructor(props) {
         super(props);
     }
     render() {
         return(
-            <div id = "recommend_style">
-                <h1>{this.props.title}</h1>
-                <p>{this.props.content}</p>
-
-            </div>
+            <>
+                <div id = "recommend_style">
+                    <h1>{this.props.title}</h1>
+                    <p>{this.props.content}</p>
+                </div>
+                <div>
+                    <div id = "style_card">
+                        <div>
+                            <HomeRecommendCard image={style_example}/>
+                            <HomeRecommendCard image={style_example}/>
+                            <HomeRecommendCard image={style_example}/>
+                            <HomeRecommendCard image={style_example}/>
+                        </div>
+                        <div>
+                            <HomeRecommendCard image={style_example}/>
+                            <HomeRecommendCard image={style_example}/>
+                            <HomeRecommendCard image={style_example}/>
+                            <HomeRecommendCard image={style_example}/>
+                        </div>
+                    </div>
+                </div>
+            </>
         )
     }
 }
