d0cc8fc1cee723ed8f43d7a44e6783b058f13240
ashjang <vkdvkdgkfla@naver.com>
2021-07-16 10:12:47 +0900

    add coordination.js and modify input.js for coordination table

---
+++ server/lib/coordination.js
@@ -0,0 +1,130 @@
+const re = /\s*(?:,|$)\s*/;
+
+const Style = Object.freeze({
+	"미니멀": 1,
+	"케쥬얼": 2,
+	"캠퍼스": 4,
+	"스트릿": 8,
+	"락시크": 16,
+	"아메카지": 32,
+	"시티보이": 64,
+	"오피스": 128,
+	"섹시글램": 256,
+	"페미닌": 512,
+	"러블리": 1024
+})
+
+function getStyleCode(text) {
+	let code = 0
+	for (const element of text.split(re)) {
+		code += Style[element]
+		if (code == NaN) {
+			console.log(element)
+			break
+		}
+	}
+	return code
+}
+
+const Gender = Object.freeze({
+	"남": 1,
+	"여": 2,
+	"남,여": 3,
+})
+
+function getGenderCode(text) {
+	let code = 0
+	for (const element of text.split(re)) {
+		code += Gender[element]
+		if (code == NaN) {
+			console.log(element)
+			break
+		}
+	}
+	return code
+}
+
+const Age = Object.freeze({
+	"10대": 1,
+	"20대": 2,
+	"30대": 4,
+	"40대": 8,
+	"50대": 16,
+})
+
+function getAgeCode(text) {
+	let code = 0
+	for (const element of text.split(re)) {
+		code += Age[element]
+		if (code == NaN) {
+			console.log(element)
+			break
+		}
+	}
+	return code
+}
+
+
+//	체형 객체, 변환함수 구현해야함
+const BodyShape = Object.freeze({
+	"하체통통": 1,
+})
+
+function getBodyShapeCode(text) {
+	let code = 0
+	for (const element of text.split(re)) {
+		code += BodyShape[element]
+		if (code == NaN) {
+			console.log(element)
+			break
+		}
+	}
+	return code
+}
+
+const Price = Object.freeze({
+	"10만원이하": 1,
+	"10만원대": 2,
+	"20만원대": 4,
+	"30만원대": 8,
+	"40만원대": 16,
+	"50만원대이상": 32,
+})
+
+function getPriceCode(text) {
+	let code = 0
+	for (const element of text.split(re)) {
+		code += Price[element]
+		if (code == NaN) {
+			console.log(element)
+			break
+		}
+	}
+	return code
+}
+
+const Season = Object.freeze({
+	"봄": 1,
+	"여름": 2,
+	"가을": 4,
+	"겨울": 8,
+})
+
+function getSeasonCode(text) {
+	let code = 0
+	for (const element of text.split(re)) {
+		code += Season[element]
+		if (code == NaN) {
+			console.log(element)
+			break
+		}
+	}
+	return code
+}
+
+exports.getStyleCode = getStyleCode
+exports.getSeasonCode = getSeasonCode
+exports.getAgeCode = getAgeCode
+exports.getBodyShapeCode = getBodyShapeCode
+exports.getPriceCode = getPriceCode
+exports.getGenderCode = getGenderCode
--- server/routes/input.js
+++ server/routes/input.js
@@ -4,6 +4,7 @@ const fs = require('fs')
 const parse = require('csv-parse')
 const connection = require('../lib/mysql')
 const product = require('../lib/product')
+const coordination = require('../lib/coordination')
 
 router.get('/', function(req, res, next) {
 	const processFile = async () => {
@@ -37,14 +38,70 @@ router.get('/', function(req, res, next) {
 				values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
 				[id, name, brand, code, gender, type, detail, color, fit, season, fiber, age, style, price, size, image[0], image[1] || null, image[2] || null],
 				(err, result) => {
-					if (err)
+					if (err) {
 						console.error(err)
+					}
 				})
 			records.push(record)
 		}
 		return records
 	}
 	
+	(async () => {
+		const records = await processFile()
+		res.send(records)
+	})()
+})
+
+router.get("/coordination", (req, res) => {
+	const processFile = async () => {
+		let records = []
+		let index = 1
+		const parser = fs
+		.createReadStream(`./product/coordination.csv`)
+		.pipe(parse({}))
+		for await(const record of parser) {
+			const id = record[0]
+			const outerID = record[1] || 0
+			const outerImageID = record[2] || 0
+			const topID = record[3] || 0
+			const topImageID = record[4] || 0
+			const bottomID = record[5] || 0
+			const bottomImageID = record[6] || 0
+			const skirtID = record[7] || 0
+			const skirtImageID = record[8] || 0
+			const setID = record[9] || 0
+			const setImageID = record[10] || 0
+			const shoesID = record[11] || 0
+			const shoesImageID = record[12] || 0
+			const bagID = record[13] || 0
+			const bagImageID = record[14] || 0
+			const hatID = record[15] || 0
+			const hatImageID = record[16] || 0
+			const title = record[17]
+			const style = coordination.getStyleCode(record[18])
+			const gender = coordination.getGenderCode(record[19])
+			const age = coordination.getAgeCode(record[20])
+			const bodyshape = coordination.getBodyShapeCode(record[21])
+			const price = coordination.getPriceCode(record[22])
+			const weather = coordination.getSeasonCode(record[23])
+			const imageID = record[24] || 0
+			// console.error(imageID)
+			connection.query("insert into coordination \
+	 			(id, title, outerID, outerImageID, topID, topImageID, bottomID, bottomImageID, skirtID, skirtImageID,\
+	 			setID,setImageID,shoesID,shoesImageID,bagID,bagImageID,hatID,hatImageID,style,gender,age,bodyshape,price,weather,imageID) \
+	 			values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?, ?, ?, ?, ?, ?,?)",
+				[id, title, outerID, outerImageID, topID, topImageID, bottomID, bottomImageID, skirtID, skirtImageID,
+					setID, setImageID, shoesID, shoesImageID, bagID, bagImageID, hatID, hatImageID, style, gender, age, bodyshape, price, weather, imageID],
+				(err, result) => {
+					if (err) {
+						console.error(err)
+					}
+				})
+			records.push(record)
+		}
+		return records
+	}
 	(async () => {
 		const records = await processFile()
 		res.send(records)
