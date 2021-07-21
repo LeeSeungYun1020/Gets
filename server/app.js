13e5b184572fdfcd1dcb87f08d5b6750ce73355c
ashjang <vkdvkdgkfla@naver.com>
2021-07-21 18:26:52 +0900

    add apis in api-center

--- server/app.js
+++ server/app.js
@@ -17,6 +17,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile)
 
+app.use(express.static('product/image'))
 app.use(logger('dev'));
 app.use(helmet());
 app.use(express.json());
