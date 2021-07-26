4326d039985a4098d1960f5caffa014034f98245
ashjang <vkdvkdgkfla@naver.com>
2021-07-26 11:51:28 +0900

    display coordination on the screen

--- server/routes/api.js
+++ server/routes/api.js
@@ -223,7 +223,35 @@ module.exports = function (passport) {
 				}
 			})
 	})
-
+	
+//코디
+	router.get("/coordination/:id", (req, res) => {
+		const id = req.params.id
+		connection.query("select * from `coordination` where `id`=?",
+			[id],
+			(err, result) => {
+				if (err || result.length === 0)
+					res.send({result: false})
+				else {
+					result[0].result = true
+					res.send(result[0])
+				}
+			})
+	})
+	
+	router.get("/coordination/image/:imageID", (req, res) => {
+		const imageID = req.params.imageID
+		const filePath = path.join(__dirname, '../coordination/image')
+		const options = {
+			root: filePath,
+		}
+		fs.promises.access(`${filePath}/${imageID}.png`, fs.constants.F_OK)
+		.then(() => res.sendFile(`${imageID}.png`, options))
+		.catch(() => res.sendFile(`${imageID}.jpg`, options, err => {
+			res.sendFile(`error.png`, options)
+		}))
+	})
+	
 // 리뷰 추가
 	router.post("/review/add/:productID", (req, res) => {
 		const productID = req.params.productID
