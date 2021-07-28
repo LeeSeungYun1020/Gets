ca9deea2ae3ed150445339198d14ae57eaf8b105
gimume <yum17201619@gmail.com>
2021-07-28 16:35:31 +0900

    modify style info css

--- web/src/components/HomeStyleInfoSlick.js
+++ web/src/components/HomeStyleInfoSlick.js
@@ -48,9 +48,7 @@ const HomeStyleInfoSlick = () => {
                 breakpoint: 1024,
                 settings: {
                     slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: true
+                    slidesToScroll: 1,
                 }
             },
             {
@@ -58,7 +56,7 @@ const HomeStyleInfoSlick = () => {
                 settings: {
                     slidesToShow: 2,
                     slidesToScroll: 2,
-                    initialSlide: 2
+                    initialSlide: 1
                 }
             },
             {
