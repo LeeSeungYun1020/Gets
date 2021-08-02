0fa21155a8aeb387e67abec0ae0a997353fea9e3
ashjang <vkdvkdgkfla@naver.com>
2021-08-02 13:46:47 +0900

    list products/coordinations in favorite

--- server/routes/closet.js
+++ server/routes/closet.js
@@ -7,5 +7,29 @@ module.exports = function (passport) {
 		res.send("closet")
 	});
 	
+	router.get('/product/list',(req,res)=>{
+		let user = req.body.email
+		connection.query(`select * from favoriteProduct where userEmail=?`,[user],
+			(err,result)=>{
+				if (err || result.length === 0)
+					res.send({result: false,error:"notMatch"})
+				else {
+					res.send(result)
+				}
+			})
+	})
+	
+	router.get('/coordination/list',(req,res)=>{
+		let user = req.body.email
+		connection.query(`select * from favoriteCoordination where userEmail=?`,[user],
+			(err,result)=>{
+				if (err || result.length === 0)
+					res.send({result: false,error:"notMatch"})
+				else {
+					res.send(result)
+				}
+			})
+	})
+	
 	return router
 }
