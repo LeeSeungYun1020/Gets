d6934d132f1667953e59e2f9b5d243ebd3550f0d
LeeSeungYun1020 <ileilliat@gmail.com>
2021-08-04 11:18:14 +0900

    Add feedback sequence

--- app/app/src/main/java/com/sys/gets/ui/account/AccountFragment.kt
+++ app/app/src/main/java/com/sys/gets/ui/account/AccountFragment.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import com.google.android.material.snackbar.Snackbar
 import com.sys.gets.R
 import com.sys.gets.sign.LoginActivity
 
@@ -14,6 +15,28 @@ class AccountFragment : PreferenceFragmentCompat() {
 
         findPreference<Preference>("signout")?.setOnPreferenceClickListener {
             startActivity(Intent(requireContext(), LoginActivity::class.java))
+            true
+        }
+
+        findPreference<Preference>("feedback")?.setOnPreferenceClickListener {
+            val email = Intent(Intent.ACTION_SEND).apply {
+                type = "message/rfc822"
+                putExtra(Intent.EXTRA_EMAIL, arrayOf("leeseungyun@pusan.ac.kr"))
+                putExtra(Intent.EXTRA_SUBJECT, getString(R.string.msg_feedback_title))
+                putExtra(
+                    Intent.EXTRA_TEXT,
+                    "${System.currentTimeMillis()}\n" + getString(R.string.msg_feedback_text)
+                )
+            }
+            try {
+                startActivity(email)
+            } catch (e: Exception) {
+                Snackbar.make(
+                    requireView(),
+                    R.string.msg_feedback_error,
+                    Snackbar.LENGTH_SHORT
+                ).show()
+            }
             true
         }
     }
