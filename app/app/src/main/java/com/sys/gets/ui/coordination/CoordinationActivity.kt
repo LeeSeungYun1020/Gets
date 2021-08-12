6705a639ea68a4d711048c7e38e52281adc0c53b
Lee Seung Yun <ileilliat@gmail.com>
2021-08-12 20:37:36 +0900

    Add CoordinationActivity layout

---
+++ app/app/src/main/java/com/sys/gets/ui/coordination/CoordinationActivity.kt
@@ -0,0 +1,12 @@
+package com.sys.gets.ui.coordination
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sys.gets.R
+
+class CoordinationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coordination)
+    }
+}
