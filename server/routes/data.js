a266f26321a5a9156e45c92388857745ccae9aac
Lee Seung Yun <ileilliat@gmail.com>
2021-11-02 23:26:16 +0900

    Add data route file comment

--- server/routes/data.js
+++ server/routes/data.js
@@ -1,13 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const path = require('path')
 const connection = require('../lib/mysql')
-const fs = require('fs')
 const spawn = require('child_process').spawn
 
 
 module.exports = function (passport) {
-	router.get('/', function (req, res, next) {
+	// 연결 확인
+	router.get('/', function (req, res) {
 		res.send('data')
 	});
 	
@@ -56,19 +55,21 @@ module.exports = function (passport) {
 		} else {
 			process = spawn('python3', ['-O', scriptPath + scriptName])
 		}
-		
+		// 성공할 경우
 		process.stdout.on('data', function (data) {
 			let json = JSON.parse(data)
 			json['result'] = true
 			console.log(json)
 			res.send(json)
 		})
+		// 실패할 경우
 		process.stderr.on('data', function () {
 			console.log({'result': false})
 			res.send({'result': false})
 		})
 	})
 	
+	// 성별에 따른 필터 적용
 	router.get('/coordination/filter/gender/:gender', (req, res) => {
 		let gender = req.params.gender
 		console.log('/data/coordination/filter/gender/' + gender)
