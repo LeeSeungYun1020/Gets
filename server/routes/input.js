8b7579273d071bbd8039fb95c6f0f9bcb36bce81
Lee Seung Yun <ileilliat@gmail.com>
2022-06-20 19:58:36 +0900

    Modify article input data format error

--- server/routes/input.js
+++ server/routes/input.js
@@ -141,27 +141,27 @@ router.get("/article", (req, res) => {
                 '편함, 일상, 익숙함', \
                 'comfort, daily, familiarity', \
                 'T.P.O(Time, Place, Occasion)에서 크게 벗어나지 않는 한도 내에서 부담 없이 입을 수 있는 자연스럽고 편한 느낌의 의류. \n\n가격대도 저가부터 고가까지 넓게 퍼져 있어서 자기 경제 사정에 맞춰 구매 가능하다. \n\n달리 말하자면, 상황에 어긋나지만 않으면 입고 싶은 것을 아무거나 매칭하여 편하게 입으면 그것이 케주얼 웨어가 된다.', \
-                'Natural and comfortable clothing that can be worn casually within the limits of T.P.O (Time, Place, Occasion). \n\nThe price range is also widely spread from low to high, so you can purchase it according to your economic situation. \n\nIn other words, as long as it doesn't go against the situation, if you wear it comfortably by matching anything you want to wear, it becomes casual wear.' \
+                'Natural and comfortable clothing that can be worn casually within the limits of T.P.O (Time, Place, Occasion). \n\nThe price range is also widely spread from low to high, so you can purchase it according to your economic situation. \n\nIn other words, as long as it does not go against the situation, if you wear it comfortably by matching anything you want to wear, it becomes casual wear.' \
                ); \
         insert into article \
         values (4, 'campus', \
                 '캐주얼, 스포티, 새내기', \
                 'casual, sporty, newbie', \
                 '캠퍼스룩은 말그대로 캠퍼스 즉 학교에서 입을만한 패션을 말하며 요즘 캠퍼스룩의 기본 베이스는 바로 ‘꾸안꾸’이다. \n\n일명 놈코어(Norm Core), 표준을 뜻하는 ‘놈(norm)’과 핵심을 뜻하는 ‘코어(core)’의 합성어로 평범함(놈)을 추구하면서도 포인트(코어)가 있는 스타일을 말한다. \n\n그중에서도 요즘은 캐주얼과 스포티즘 룩(Sportism Look)에 쓰이는 아이템들을 활용한 스타일들이 많이 보이고 있는 추세이다.', \
-                'Campus look literally refers to fashion that can be worn on campus, that is, at school. \n\nNorm Core, a compound word of 'norm' meaning standard and 'core' meaning core, refers to a style with a point (core) while pursuing normality (norm). \n\nAmong them, there are a lot of styles using items used for casual and sportism look these days.'); \
+                'Campus look literally refers to fashion that can be worn on campus, that is, at school. \n\nNorm Core, a compound word of ''norm'' meaning standard and ''core'' meaning core, refers to a style with a point (core) while pursuing normality (norm). \n\nAmong them, there are a lot of styles using items used for casual and sportism look these days.'); \
         insert into article \
         values (8, 'street', \
                 '젊음, 길거리, 트렌드', \
                 'young, street, trend', \
                 '‘스트리트 패션(Street Fashion)’ 말 그대로 길거리 사람들의 패션을 일컫는 말이다. \n\n그리고 거리의 유행 현상/ 패션 트렌드를 말하기도 한다. \n\n젊은이들의 길거리 유행으로 시작되는 패션 분야이기 때문에 정의는 상당히 애매하다. \n\n또한 유행을 따라가기 때문에 어느 시점에서는 어떤 아이템이 스트리트 패션이라고 불리다가 그렇지 않게 되는 경우도 있고, 갑자기 스트릿 패션으로 불릴 수도 있다. \n\n그래서 다양한 의미로 해석할 수 있고 그에 따라 다양한 종류의 룩이 파생된다.',
-                '“Street Fashion” literally refers to the fashion of street people. \n\nThey also talk about street fashion/fashion trends. \n\nThe definition is quite ambiguous because it is a fashion field that starts with young people's street fashion. \n\nAlso, because it follows the trend, at some point an item is called street fashion, and sometimes it is not, or suddenly it is called street fashion. \n\nTherefore, it can be interpreted in various meanings, and various kinds of looks are derived accordingly.' \
+                '“Street Fashion” literally refers to the fashion of street people. \n\nThey also talk about street fashion/fashion trends. \n\nThe definition is quite ambiguous because it is a fashion field that starts with young people''s street fashion. \n\nAlso, because it follows the trend, at some point an item is called street fashion, and sometimes it is not, or suddenly it is called street fashion. \n\nTherefore, it can be interpreted in various meanings, and various kinds of looks are derived accordingly.' \
                ); \
         insert into article \
         values (16, 'rock_chic', \
                 '터프, 시크, 섹시', \
                 'tough, chic, sexy', \
                 '락(Rock)과 시크(Chic)의 합성어로 자유분방하고 터프한 느낌을 주는 스타일을 칭하는 용어다. \n\n보통은 거칠고 투박한 느낌이지만 뒤에 붙어 있는 ‘시크’의 영향으로 도도하고 멋진 이미지까지 포함한다. \n\n격동적 변화의 시기였던 70년대를 주름잡았던 락은 본능적이고 야성적인 매력을 느낄 수 있는 음악이었다. \n\n그와 같이 락시크 스타일 또한 거칠면서도 세련되고 섹시한 스타일을 동시에 추구하는 패션계의 까칠하고 영민한 반항아 격이다.', \
-                'It is a compound word of rock and chic, and refers to a style that gives a free-spirited and tough feeling. \n\nIt usually feels rough and crude, but it also includes a pompous and cool image due to the influence of the 'chic' attached to the back. \n\nRock, which dominated the 70s, a period of turbulent change, was music that could feel instinctive and wild charm. \n\nAs such, the rock chic style is also a tough and agile rebel in the fashion world that pursues a rough, sophisticated and sexy style at the same time.' \
+                'It is a compound word of rock and chic, and refers to a style that gives a free-spirited and tough feeling. \n\nIt usually feels rough and crude, but it also includes a pompous and cool image due to the influence of the ''chic'' attached to the back. \n\nRock, which dominated the 70s, a period of turbulent change, was music that could feel instinctive and wild charm. \n\nAs such, the rock chic style is also a tough and agile rebel in the fashion world that pursues a rough, sophisticated and sexy style at the same time.' \
                ); \
         insert into article \
         values (32, 'amekaji', \
