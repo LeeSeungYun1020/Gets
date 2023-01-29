473e1ffa506bbe4f0e88d5b116ca907015fbfaf5
Lee Seung Yun <ileilliat@gmail.com>
2023-01-29 18:41:17 +0900

    Modify app networking settings

--- app/app/src/main/java/com/sys/gets/network/Network.kt
+++ app/app/src/main/java/com/sys/gets/network/Network.kt
@@ -23,7 +23,7 @@ class Network(context: Context) {
                 }
             }
 
-        const val BASE_URL = "http://52.231.193.69:3000" // "http://10.0.2.2:3000"
+        const val BASE_URL = "http://10.0.2.2:3000"
         const val API_URL = "$BASE_URL/api"
         const val SIGN_IN_URL = "$BASE_URL/auth/signin"
         const val SIGN_OUT_URL = "$BASE_URL/auth/signout"
